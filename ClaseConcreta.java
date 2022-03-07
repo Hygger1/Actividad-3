ad130ef817e34fdfe4d975d3931bbad64b25549a
Oscar Solano <100371859+Hygger1@users.noreply.github.com>
2022-03-06 21:13:11 -0600

    Add files via upload

---
+++ ClaseConcreta.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.template;
+
+/**
+ *
+ * @author angul
+ */
+public class ClaseConcreta extends ClaseAbstracta {
+
+    public ClaseConcreta() {
+    }
+    @Override
+    public int multiplicar(int num) {
+        return num * 100;
+    }
+
+    // ------------------------------------
+    @Override
+    public int sumar(int num) {
+        return num + 10;
+    }
+}
